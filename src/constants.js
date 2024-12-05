fd6e3e96758faaf4c4736b3d225a65ae99473c23
Houssem Eddine ELGAIED <houssemelgaied@gmail.com>
2024-12-05 12:55:50 +0100

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,8 @@
 //export const BASE_URL =
  //process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '';
 export const BASE_URL = '';
-export const PRODUCTS_URL = '/api/v1/products';
-export const UPLOAD_URL = '/api/v1/upload';
-export const USERS_URL = '/api/v1/users';
-export const ORDERS_URL = '/api/v1/orders';
-export const RAZORPAY_URL = '/api/v1/payment';
+export const PRODUCTS_URL = 'https://backend-ecommerce-1-a61w.onrender.com/api/v1/products';
+export const UPLOAD_URL = 'https://backend-ecommerce-1-a61w.onrender.com/api/v1/upload';
+export const USERS_URL = 'https://backend-ecommerce-1-a61w.onrender.com/api/v1/users';
+export const ORDERS_URL = 'https://backend-ecommerce-1-a61w.onrender.com/api/v1/orders';
+export const RAZORPAY_URL = 'https://backend-ecommerce-1-a61w.onrender.com/api/v1/payment';
