dccee6a86c0c37fab62a895c28d36c10817b4f27
Houssem Eddine ELGAIED <houssemelgaied@gmail.com>
2024-12-05 12:51:55 +0100

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 //export const BASE_URL =
  //process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '';
-export const BASE_URL = 'https://backend-ecommerce-1-a61w.onrender.com';
+export const BASE_URL = '';
 export const PRODUCTS_URL = '/api/v1/products';
 export const UPLOAD_URL = '/api/v1/upload';
 export const USERS_URL = '/api/v1/users';
